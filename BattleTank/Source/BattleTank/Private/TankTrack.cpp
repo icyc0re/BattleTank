f4a5aeddcf6fab09e44fc35cde01636b43c4b46d
tiz <tiziano.signo@gmail.com>
2017-11-03 00:55:48 +0100

    add tracks base c++ object and basic movement of the tracks

---
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTrack.h"
+
+#include "Components/PrimitiveComponent.h"
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	FVector ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	FVector ForceLocation = GetComponentLocation();
+
+	UPrimitiveComponent* TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+}
